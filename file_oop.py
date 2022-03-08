54330792b3f7638c4dbcf3791676cbc0ce7ae927
ganangrizki <81669435+ganangrizki@users.noreply.github.com>
2022-03-08 08:26:41 +0700

    sesi pagi dan sore

---
+++ file_oop.py
@@ -0,0 +1,22 @@
+from abc import abstractmethod, ABC
+
+class Hewan(ABC):
+    
+    @abstractmethod
+    def caramemakan(self):
+        pass
+    
+class Harimau(Hewan):
+    def caramemakan(self):
+        print("Menjatuhkan ke tanah dan mematahkan atau menggigit lehernya hingga mati.\n")
+        
+class Buaya(Hewan):
+    def caramemakan(self):
+        print("cara makanya dengan menerkam sekaligus menggigit mangsanya kemudian menarik dengan kuat dan tiba-tiba ke air.")
+       
+    harimau1=Harimau
+    (buaya1=Buaya()
+
+    harimau1.caramemakan()
+    buaya1.caramemakan()
+     
